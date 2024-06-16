46605355062cc61e1891c67fed685a9c60bbb955
pushkar1713 <pushkar1713@gmail.com>
2024-06-16 10:36:59 +0530

    finish setting up react project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorComp />,
+    children: [
+      {
+        path: "/signup", // parentPath/{path} => localhost:1244/about
+        element: <Signup />,
+        children: [
+          {
+            path: "signin",
+            element: <Signin />, // parentPath/{path} => localhost:1244/about/profile
+          },
+        ],
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+
+      {
+        path: "/send",
+        element: <SendMoney />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={appRouter} />
+  </React.StrictMode>
+);
